c836fe600ba19aa168719a8864e089965e3215e7
Zeztron <pateltharsh@gmail.com>
2022-11-22 09:57:30 -0600

    Added posting comment functionality

--- components/NoResults.tsx
+++ components/NoResults.tsx
@@ -1,12 +1,24 @@
-import { NextPage } from 'next';
 import React from 'react';
+import { MdOutlineVideocamOff } from 'react-icons/md';
+import { BiCommentX } from 'react-icons/bi';
 
 interface NoResultsProps {
   text: string;
 }
 
-const NoResults: NextPage<NoResultsProps> = ({ text }) => {
-  return <div>{text}</div>;
+const NoResults = ({ text }: NoResultsProps) => {
+  return (
+    <div className="flex flex-col justify-center items-center h-full w-full">
+      <p className="text-8xl">
+        {text === 'No comments yet!' ? (
+          <BiCommentX />
+        ) : (
+          <MdOutlineVideocamOff />
+        )}
+      </p>
+      <p className="text-2xl text-center">{text}</p>
+    </div>
+  );
 };
 
 export default NoResults;
--- pages/detail/[id].tsx
+++ pages/detail/[id].tsx
@@ -21,6 +21,9 @@ const Detail = ({ postDetails }: DetailProps) => {
   const [post, setPost] = useState(postDetails);
   const [playing, setPlaying] = useState(false);
   const [isMuted, setIsMuted] = useState(false);
+  const [comment, setComment] = useState('');
+  const [isPostingComment, setIsPostingComment] = useState(false);
+
   const router = useRouter();
   const { userProfile }: any = useAuthStore();
 
@@ -51,6 +54,23 @@ const Detail = ({ postDetails }: DetailProps) => {
       });
 
       setPost({ ...post, likes: data.likes });
+    }
+  };
+
+  const addComment = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (userProfile && comment) {
+      setIsPostingComment(true);
+
+      const { data } = await axios.put(`${BASE_URL}/api/post/${post._id}`, {
+        userId: userProfile._id,
+        comment,
+      });
+
+      setPost({ ...post, comments: data.comment });
+      setComment('');
+      setIsPostingComment(false);
     }
   };
 
@@ -135,7 +155,13 @@ const Detail = ({ postDetails }: DetailProps) => {
               />
             )}
           </div>
-          <Comments />
+          <Comments
+            comment={comment}
+            setComment={setComment}
+            addComment={addComment}
+            comments={post.comments}
+            isPostingComment={isPostingComment}
+          />
         </div>
       </div>
     </div>
